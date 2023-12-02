1ef2c81280bc314a735fcda4851f7a02980b1795
devops <ssarker@sysonex.dev>
2023-12-03 00:35:25 +0600

    Dotnet demo MVC app initialization

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace myfirstdotnetproject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
